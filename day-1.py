d8237e7309d6f3ccec14ef3a580c92997a75143d
navyapolsani20 <navyapolsani20@gmail.com>
2026-07-28 14:45:02 +0530

    Tokens-Day1

---
+++ day-1.py
@@ -0,0 +1,11 @@
+print("Hello Codegnan")
+name = "Navya"
+print(name)
+email_id ="navyapolsani20@gmail.com"
+print(email_id)
+navya = "1"
+print(navya)
+name,location,age = "navya","hyderabad","7"
+print(name)
+print(location)
+print(age)
