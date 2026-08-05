9d9c2856dcbd4de4e130cba14fb4c019963f6148
navyapolsani20 <navyapolsani20@gmail.com>
2026-08-05 22:32:29 +0530

    strings-task

---
+++ task2.py
@@ -0,0 +1,20 @@
+#task-1:
+name = "Polsani Navya Rao"
+print(len(name))
+#postive-positive
+print(name[0:])
+print(name[4:14])
+print(name[8:17])
+#negative-negative
+print(name[-13:-1])
+print(name[:-7])
+print(name[-10:])
+#positive-negative
+print(name[-5:16])
+print(name[-10:15])
+
+#task-2:
+for i in range(65,91):
+    print(chr(i),end = " ")
+
+
