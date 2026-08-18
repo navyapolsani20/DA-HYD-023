53cf0cc3ec6a071aa82ebe371b3446dc8c68c448
navyapolsani20 <navyapolsani20@gmail.com>
2026-08-18 13:02:10 +0530

    functions

---
+++ practice/day_16_task.py
@@ -0,0 +1,19 @@
+personal_info = {"Student Name":"Polsani Navya Rao",
+                 "Student Id":"CGH3963","Batch No":"DA-HYD-023",
+                 "Email Id":"navyapolsani20@gmail.com",
+                 "Date of Birth":"2004-04-20","Age":"22",
+                 "Gender":"Female","City":"Mulug",
+                 "State":"Telangana","Student Phone Number":"7569591568",
+                 "Parent Phone Number":"8555947742",
+                 "Github Link":"https://github.com/navyapolsani20"}
+print(personal_info)
+academic_info = {"College Name":"CMR Technical Campus",
+                 "USN Number":"227r1a67b1","Qualificatin":"UG",
+                 "Department":"CSE-DS","Pass Out Year":"2026",
+                 "Graduation Percentage":"80%","Arrears":"No",
+                 "10th Pass year":"2020","10th Percenatge":"100%",
+                 "12th Pass Year":"2022","12th Percentage":"98%",
+                 "Skills":"Python,MYSQL,Statistics,numpy,pandas,Data analytics"}
+print(academic_info)
+print(personal_info.keys())
+print(academic_info.keys())
